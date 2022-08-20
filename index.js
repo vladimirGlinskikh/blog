be1b1c8532971d2f279a3fc5ac7d3a925aabc359
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-08-20 18:31:27 +0300

    Created tested authorisation

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Start project!');
+});
+
+app.post('/auth/login', (req, res) => {
+    console.log(req.body);
+    res.json({
+        success: true,
+    });
 });
 
 app.listen(4444, (err) => {
