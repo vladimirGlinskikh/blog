2a22a314369d913d40af977d7d21f53f6159a565
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-08-20 19:42:03 +0300

    Connection JSON Web Token

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import jwt from "jsonwebtoken";
 
 const app = express();
 
@@ -10,8 +11,15 @@ app.get('/', (req, res) => {
 
 app.post('/auth/login', (req, res) => {
     console.log(req.body);
+
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: 'Vladimir Glinskikh',
+    }, 'secret123',);
+
     res.json({
         success: true,
+        token,
     });
 });
 
