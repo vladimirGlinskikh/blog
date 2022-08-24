81de1daed585556999a6be7a344d98bff9e8730a
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-08-24 19:05:22 +0300

    Creating functionality for articles (CRUD)

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import {registerValidation} from "./validations/auth.js";
+import {registerValidation, loginValidation, postCreateValidation} from "./validations.js";
 import checkAuth from "./utils/checkAuth.js";
-import * as UserController from './controllers/userController.js';
+import * as UserController from './controllers/UserController.js';
+import * as PostController from './controllers/PostController.js';
+import Post from "./models/Post.js";
 
 mongoose
     .connect('mongodb://localhost:27017/blogdb')
@@ -13,11 +15,18 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/login', UserController.login);
+app.post('/auth/login', loginValidation, UserController.login);
 
 app.post('/auth/register', registerValidation, UserController.register);
 
 app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', PostController.update);
+
 
 app.listen(4444, (err) => {
     if (err) {
