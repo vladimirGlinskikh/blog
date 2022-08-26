e305fe260cc44957b7e2dc0ae203fed20a068696
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-08-26 20:07:00 +0300

    Refactor the final backend code

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ import express from 'express';
 import multer from 'multer';
 import mongoose from 'mongoose';
 import {registerValidation, loginValidation, postCreateValidation} from "./validations.js";
-import checkAuth from "./utils/checkAuth.js";
-import * as UserController from './controllers/UserController.js';
-import * as PostController from './controllers/PostController.js';
-import handleValidationErrors from "./utils/handleValidationErrors.js";
+import {handleValidationErrors, checkAuth} from "./utils/index.js";
+import {UserController, PostController} from './controllers/index.js';
 
 mongoose
     .connect('mongodb://localhost:27017/blogdb')
