cdf20de36b031f03e269df33651d97ea002a022e
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-08-27 15:01:51 +0300

    Install cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import multer from 'multer';
+import cors from 'cors';
 import mongoose from 'mongoose';
 import {registerValidation, loginValidation, postCreateValidation} from "./validations.js";
 import {handleValidationErrors, checkAuth} from "./utils/index.js";
@@ -24,6 +25,7 @@ const storage = multer.diskStorage({
 const upload = multer({storage});
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
