3107b6063ce0325d668554e0e4f55b817a752cca
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-09-01 16:43:38 +0300

    Corrected the error with quotation marks in app.post() routes

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/auth/me', checkAuth, UserController.getMe);
 
 app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
     res.json({
-        url: '/uploads/${req.file.originalname}',
+        url: `/uploads/${req.file.originalname}`,
     });
 });
 
